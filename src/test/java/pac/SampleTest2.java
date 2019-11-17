8469aa97e24b7541969e4d3534f2b76471989933
Uday <Admin@192.168.0.100>
2019-11-17 11:08:23 +0530

    push

---
+++ src/test/java/pac/SampleTest2.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package pac;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author deepak
+ *
+ */
+public class SampleTest2 {
+	@Test
+	public void creatreUserTest() {
+		System.out.println("execute creatreUserTest");
+	}
+	@Test
+	public void modifyuserTest() {
+		System.out.println("execute modifyuserTest");
+	}
+	@Test
+	public void deleteuserTest() {
+		System.out.println("execute deleteuserTest");
+	}
+}
