c022b53647e47913c02f0953697379f06707a6c3
Uday <Admin@192.168.0.100>
2019-11-17 10:21:31 +0530

    push framework

---
+++ src/test/java/pac/SampleTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package pac;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author deepak
+ *
+ */
+public class SampleTest {
+	@Test
+	public void createCampaignTest() {
+		System.out.println("execute createCampaignTest");
+	}
+	@Test
+	public void modifyCampaignTest() {
+		System.out.println("execute modifyCampaignTest");
+	}
+	@Test
+	public void deleteCampaignTest() {
+		System.out.println("execute deleteCampaignTest");
+	}
+}
